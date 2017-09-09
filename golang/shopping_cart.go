225fc1b77646b779fac70e119ec171839572331f
Felix Tjandrawibawa <cemeng@gmail.com>
2017-09-09 18:03:19 +1000

    Formatting fix after installing vim-go

--- golang/shopping_cart.go
+++ golang/shopping_cart.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-  "fmt"
-  "errors"
+	"errors"
+	"fmt"
 )
 
 type ShoppingCart struct {
-  Items []Product
-  PricingRule PricingRules
+	Items       []Product
+	PricingRule PricingRules
 }
 
 func (s *ShoppingCart) add(item Product) []Product {
-  result := append(s.Items, item)
-  return result
+	result := append(s.Items, item)
+	return result
 }
 
 func NewShoppingCart(pricingRule PricingRules) ShoppingCart {
-  shoppingCart := ShoppingCart{}
-  shoppingCart.PricingRule = pricingRule
-  return shoppingCart
+	shoppingCart := ShoppingCart{}
+	shoppingCart.PricingRule = pricingRule
+	return shoppingCart
 }
 
 type Product struct {
-  Code string
-  Name string
-  Price float32
+	Code  string
+	Name  string
+	Price float32
 }
 
 type PricingRules struct {
-  Pricing []Product
+	Pricing []Product
 }
 
 func NewPricingRules() PricingRules {
-  pricingRule := PricingRules{}
-  pricingRule.Pricing = []Product{
-    Product { Code: "ult_small", Name: "Unlimited 1GB", Price: 24.9, },
-    Product { Code: "ult_medium", Name: "Unlimited 2GB", Price: 29.9, },
-    Product { Code: "ult_large", Name: "Unlimited 5GB", Price: 44.9, },
-    Product { Code: "1gb", Name: "1 GB Data-pack", Price: 9.9, },
-  }
-  return pricingRule
+	pricingRule := PricingRules{}
+	pricingRule.Pricing = []Product{
+		Product{Code: "ult_small", Name: "Unlimited 1GB", Price: 24.9},
+		Product{Code: "ult_medium", Name: "Unlimited 2GB", Price: 29.9},
+		Product{Code: "ult_large", Name: "Unlimited 5GB", Price: 44.9},
+		Product{Code: "1gb", Name: "1 GB Data-pack", Price: 9.9},
+	}
+	return pricingRule
 }
 
 func (p *PricingRules) findPricingByCode(code string) (Product, error) {
-  for _, pricing := range p.Pricing {
-     if pricing.Code == code {
-         return pricing, nil
-     }
-  }
-  return Product{}, errors.New("Pricing not found")
+	for _, pricing := range p.Pricing {
+		if pricing.Code == code {
+			return pricing, nil
+		}
+	}
+	return Product{}, errors.New("Pricing not found")
 }
 
 func main() {
-  pricingRule := NewPricingRules()
-  // fmt.Println(pricingRule.Pricing)
-  fmt.Println(pricingRule.findPricingByCode("ult_small"))
-  shoppingCart := NewShoppingCart(pricingRule)
-  fmt.Println(shoppingCart)
+	pricingRule := NewPricingRules()
+	// fmt.Println(pricingRule.Pricing)
+	fmt.Println(pricingRule.findPricingByCode("ult_small"))
+	shoppingCart := NewShoppingCart(pricingRule)
+	fmt.Println(shoppingCart)
 }
