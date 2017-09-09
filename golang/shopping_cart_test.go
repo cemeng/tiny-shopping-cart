49e46aced777f0df3f9466c30fbdc9fe5d9f758f
Felix Tjandrawibawa <cemeng@gmail.com>
2017-09-09 20:43:49 +1000

    Preparing for bundle rule

--- golang/shopping_cart_test.go
+++ golang/shopping_cart_test.go
@@ -6,7 +6,9 @@ func TestShoppingCart(t *testing.T) {
 	pricingRule := NewPricingRules()
 	shoppingCart := NewShoppingCart(pricingRule)
 	ultSmall, _ := pricingRule.findPricingByCode("ult_small")
+	ultMedium, _ := pricingRule.findPricingByCode("ult_medium")
 	ultLarge, _ := pricingRule.findPricingByCode("ult_large")
+	dataPack, _ := pricingRule.findPricingByCode("1gb")
 
 	shoppingCart.add(ultSmall)
 	if len(shoppingCart.Items) != 1 {
@@ -33,4 +35,8 @@ func TestShoppingCart(t *testing.T) {
 	if shoppingCart.total() != Round(39.90*4, 2) {
 		t.Errorf("Total unlimited 5GB sims bulk discount, expecting %f but got %f", Round(39.90*4, 2), shoppingCart.total())
 	}
+
+	shoppingCart.clear()
+	shoppingCart.add(ultMedium)
+	// We will bundle in a free 1 GB Data-pack free-of-charge with every Unlimited 2GB sold
 }
